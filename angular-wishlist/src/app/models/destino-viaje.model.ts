84ca82f9f06d29f2f72e3392fefc55e136fc5417
Tanya García <garcia.tanya0204@gmail.com>
2020-10-12 18:53:59 -0300

    Ejercicios terminados Semana 1

---
+++ angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -0,0 +1,10 @@
+export class DestinoViaje{
+    nombre:string;
+    imagenUrl:string;
+
+    constructor(nombre:string, url:string)
+    {
+        this.nombre = nombre;
+        this.imagenUrl = url;
+    }
+}
